b60b9efaf8c422b29cdce7e3316bf3cc7a7cbee3
MayDay9939 <lowhongshengjovian@gmail.com>
2024-03-11 00:09:44 +0800

    added search bar, fixed bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,8 @@ import axios from "axios";
 import {useNavigate} from 'react-router-dom';
 import FoodItemCard from "../components/FoodItemCard";
 import NoFoodItemCard from "../components/NoFoodItemCard";
-import config from '../chatbot/config.js';
-import MessageParser from '../chatbot/MessageParser.js';
-import ActionProvider from '../chatbot/ActionProvider.js';
-import Chatbot from "react-chatbot-kit";
 
-const {Title} = Typography;
+const { Title } = Typography;
 const { Search } = Input;
 const pageSize = 6; 
 
@@ -17,92 +13,58 @@ const rowStyle = {
     margin: '0 0 0 10px', 
   };
 
-const fetchFoodData = async (setFood, setLoading, token) => {
-    try {
-        console.log('Token:', token); 
-
-        const config = token ? {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        } : {};
-
-        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/food/`, config);
-        if (process.env.NODE_ENV === 'development') {
-            console.log('Response:', response.data);
-        }
-        setFood(response.data);
-    } catch (error) {
-        if (process.env.NODE_ENV === 'development') {
-            console.error('Error fetching data:', error);
-        }
-        if (error.response && error.response.status === 401) {
-            message.error('Failed to fetch food items. Please try again.');
-        }
-    } finally {
-        setLoading(false);
-    }
-};
-
-const renderFoodItems = (food) => {
-    return food.map((item, index) => (<Col key={index} span={8}>
-        <FoodItemCard foodItem={item}/>
-    </Col>));
-};
-
 export default function Home() {
     const [food, setFood] = useState([]);
+    const [filteredFoodItems, setFilteredFoodItems] = useState([]);
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
     const [currentPage, setCurrentPage] = useState(1);
     const [searchTerm, setSearchTerm] = useState('');
 
-    // get data from local storage
     const token = localStorage.getItem('token');
-    const refresh = localStorage.getItem('refresh');
 
-    if (process.env.NODE_ENV === 'development') {
-        console.log('Token:', token);
-        console.log('Refresh:', refresh);
-    }
-
-    const indexOfLastFoodItem = currentPage * pageSize;
-    const indexOfFirstFoodItem = indexOfLastFoodItem - pageSize;
-    const currentFoodItems = food.slice(indexOfFirstFoodItem, indexOfLastFoodItem);
-
-    // Handle page change
-    const handlePageChange = page => {
-        setCurrentPage(page);
-    };
+    // if (process.env.NODE_ENV === 'development') {
+    //     console.log('Token:', token);
+    //     console.log('Refresh:', refresh);
+    // }
 
     useEffect(() => {
         if (!token) {
             navigate('/login');
             return;
         }
-        axios.get(`${process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000'}/user/status/`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-            .then(statusResponse  => {
-                console.log(statusResponse.data.role);
-            })
-            .catch(statusError  => {
-                // Handle error
-                console.error('Error getting user status:', statusError);
-                navigate('/login');
-            });
-        fetchFoodData(setFood, setLoading, token, searchTerm);
-    }, []);
-    
-    const onSearch = (value) => {
-        setSearchTerm(value);
+        fetchFoodData(setFood, setLoading, token);
+    }, [token]);
+
+    useEffect(() => {
+        // Filter food items in real-time as the searchTerm changes
+        const filteredItems = food.filter(item =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredFoodItems(filteredItems);
+        setCurrentPage(1); // Reset to first page whenever the search term changes
+    }, [searchTerm, food]);
+
+    const handlePageChange = page => {
+        setCurrentPage(page);
     };
 
-    const filteredFoodItems = food.filter(item =>
-        item.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    // Adjusted to include onChange event for real-time filtering
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    // Function to handle search submissions
+    const onSearch = (value) => {
+        // Since real-time filtering is already handled by onChange, you might not need to do anything here.
+        // But if you have a specific need to handle the search submission (e.g., logging, analytics, etc.), you can do so here.
+        console.log('Search submitted:', value);
+        // For instance, you might want to filter based on a more complex criteria or navigate to another route.
+    };
+
+    const indexOfLastFoodItem = currentPage * pageSize;
+    const indexOfFirstFoodItem = indexOfLastFoodItem - pageSize;
+    const currentFoodItems = filteredFoodItems.slice(indexOfFirstFoodItem, indexOfLastFoodItem);
 
     return (
         <div>
@@ -110,21 +72,21 @@ export default function Home() {
             <div style={{ display: 'flex', justifyContent: 'center', margin: '16px 0' }}>
                 <Search
                     placeholder="Search food items"
-                    onSearch={onSearch}
+                    onChange={handleSearchChange} // Added for real-time search functionality
+                    onSearch={onSearch} // Kept for potentially other search-related operations
                     enterButton
-                    size="large"
-                    style={{
-                        maxWidth: '600px', 
-                        borderRadius: '5px', 
-                        border: '1px solid #D9E8F5', 
-                    }}
+                    style={{ width: 500, height: 40 }} 
                 />
             </div>
-            <Row gutter={16} style={{ margin: '0 16px' }}> 
+            <Row gutter={16} style={{ margin: '0 16px' }}>
                 {loading ? (
                     <Spin size="large" style={{ display: 'block', margin: '0 auto' }} />
-                ) : filteredFoodItems.length > 0 ? (
-                    renderFoodItems(filteredFoodItems)
+                ) : currentFoodItems.length > 0 ? (
+                    currentFoodItems.map((item, index) => (
+                        <Col key={index} span={8}>
+                            <FoodItemCard foodItem={item} />
+                        </Col>
+                    ))
                 ) : (
                     <NoFoodItemCard />
                 )}
@@ -141,4 +103,20 @@ export default function Home() {
             )}
         </div>
     );
+}
+
+async function fetchFoodData(setFood, setLoading, token) {
+    try {
+        const config = token ? {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        } : {};
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/food/`, config);
+        setFood(response.data);
+    } catch (error) {
+        message.error('Failed to fetch food items. Please try again.');
+    } finally {
+        setLoading(false);
+    }
 }
