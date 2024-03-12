24153e86fac396a78c5f524f391e8a8f3e7381e4
MayDay9939 <lowhongshengjovian@gmail.com>
2024-03-12 23:08:33 +0800

    updated no food item layout and profile page layout

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Card, Form, Input, message, Row, Select, Modal, Typography } from 'antd';
+import { Button, Card, Form, Input, message, Row, Select, Modal, Typography, Col, Divider } from 'antd';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import LayoutComponent from '../components/Layout';
@@ -9,15 +9,15 @@ import styled from 'styled-components';
 const { Option } = Select;
 const { Title } = Typography;
 
-// Styled components for better style management
 const StyledCard = styled(Card)`
   background-color: #f8f9fa;
   color: #343a40;
-  padding: 20px;
+  padding: 40px;
   border-radius: 10px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  margin-bottom: 20px;
-  text-align: center;
+  margin: 40px auto;
+  text-align: left;
+  max-width: 600px; 
 `;
 
 const DeleteButton = styled(Button)`
@@ -112,81 +112,83 @@ const MyProfile = () => {
 
     return (
         <LayoutComponent>
-            <Title level={2} style={{ textAlign: 'center' }}>My Profile</Title>
-            <Row gutter={16}>
-                <StyledCard>
-                    {dataFetched && (
-                        <Form
-                            form={form}
-                            name="profile"
-                            onFinish={onFinish}
-                            initialValues={formData}
-                            scrollToFirstError
-                        >
-                            <Form.Item
-                                name="username"
-                                label="Username"
-                                rules={[{ required: true, message: 'Username is required!' }]}
+                <Row justify="center" style={{ paddingTop: '20px' }}>
+                    <Col xs={24} sm={20} md={16} lg={12} xl={10}>
+                        <Title level={2} style={{ textAlign: 'center' }}>My Profile</Title>
+                        <StyledCard>
+                        {dataFetched && (
+                            <Form
+                                form={form}
+                                name="profile"
+                                onFinish={onFinish}
+                                initialValues={formData}
+                                scrollToFirstError
                             >
-                                <Input disabled />
-                            </Form.Item>
-    
-                            <Form.Item
-                                name="firstName"
-                                label="First Name"
-                                rules={[{ required: true, message: 'First Name is required!' }]}
-                            >
-                                <Input />
-                            </Form.Item>
-    
-                            <Form.Item
-                                name="lastName"
-                                label="Last Name"
-                                rules={[{ required: true, message: 'Last Name is required!' }]}
-                            >
-                                <Input />
-                            </Form.Item>
-    
-                            <Form.Item
-                                name="email"
-                                label="Email"
-                                rules={[{ required: true, message: 'Email is required!', type: 'email' }]}
-                            >
-                                <Input />
-                            </Form.Item>
+                                <Form.Item
+                                    name="username"
+                                    label="Username"
+                                    rules={[{ required: true, message: 'Username is required!' }]}
+                                >
+                                    <Input disabled />
+                                </Form.Item>
+        
+                                <Form.Item
+                                    name="firstName"
+                                    label="First Name"
+                                    rules={[{ required: true, message: 'First Name is required!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+        
+                                <Form.Item
+                                    name="lastName"
+                                    label="Last Name"
+                                    rules={[{ required: true, message: 'Last Name is required!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+        
+                                <Form.Item
+                                    name="email"
+                                    label="Email"
+                                    rules={[{ required: true, message: 'Email is required!', type: 'email' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
 
-                            <Form.Item
-                                name="role"
-                                label="Role"
-                                rules={[{ required: true, message: 'Role is required!' }]}
-                            >
-                                <Input disabled />
-                            </Form.Item>
-    
-                            <Form.Item
-                                name="phoneNumber"
-                                label="Phone Number"
-                                rules={[{ required: true, message: 'Phone Number is required!' }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                            
-                            <Form.Item>
-                                <Button type="primary" onClick={() => showModal(false)} htmlType="button" style={{ marginRight: '8px' }}>
-                                    Update Profile
-                                </Button>
-                                <DeleteButton onClick={() => showModal(true)}>
-                                    Delete Profile
-                                </DeleteButton>
-                            </Form.Item>
-    
-                            <Modal title={isDeleteMode ? "Delete Profile Confirmation" : "Update Profile Confirmation"} visible={isModalVisible} onOk={handleModalOk}
-                                   onCancel={handleModalCancel}>
-                                <p>{isDeleteMode ? "Are you sure you want to delete your profile?" : "Are you sure you want to update your profile?"}</p>
-                            </Modal>
-                        </Form>
-                    )}
-                </StyledCard>
+                                <Form.Item
+                                    name="role"
+                                    label="Role"
+                                    rules={[{ required: true, message: 'Role is required!' }]}
+                                >
+                                    <Input disabled />
+                                </Form.Item>
+        
+                                <Form.Item
+                                    name="phoneNumber"
+                                    label="Phone Number"
+                                    rules={[{ required: true, message: 'Phone Number is required!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+                                
+                                <Form.Item>
+                                    <Button type="primary" onClick={() => showModal(false)} htmlType="button" style={{ marginRight: '8px' }}>
+                                        Update Profile
+                                    </Button>
+                                    <DeleteButton onClick={() => showModal(true)}>
+                                        Delete Profile
+                                    </DeleteButton>
+                                </Form.Item>
+        
+                                <Modal title={isDeleteMode ? "Delete Profile Confirmation" : "Update Profile Confirmation"} visible={isModalVisible} onOk={handleModalOk}
+                                    onCancel={handleModalCancel}>
+                                    <p>{isDeleteMode ? "Are you sure you want to delete your profile?" : "Are you sure you want to update your profile?"}</p>
+                                </Modal>
+                            </Form>
+                        )}
+                    </StyledCard>
+                </Col>
             </Row>
         </LayoutComponent>
     );    
